b10321db373cde1eefa34cb9f1986984e793be6e
21KennethTran <43259774+21KennethTran@users.noreply.github.com>
2022-04-22 16:41:38 -0700

    Update MarkdownParseTest.java
    javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class MarkdownParseTest {
-    @Test
+    @Test //tester
     public void addition() {
         assertEquals(2, 1 + 1);
     }
